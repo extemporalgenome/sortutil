27e60e8895fd50860a0937b087c8450752fa1d21
Kevin Gillette <extemporalgenome@gmail.com>
2013-01-23 13:41:53 -0700

    Refined Log: now uses Mark, tunes output to Len.

--- wrappers.go
+++ wrappers.go
@@ -24,37 +24,73 @@ func (s *Stat) Less(i, j int) bool { s.N.Less++; return s.I.Less(i, j) }
 func (s *Stat) Swap(i, j int)      { s.N.Swap++; s.I.Swap(i, j) }
 func (s *Stat) String() string     { return fmt.Sprintf("%+v", s.N) }
 
+// Mark should produce output with the same visible length that fmt.Sprint
+// would produce when passed the receiver. Within the same alignment
+// constraints, the returned string should emphasize the indices i and j.
+// This is an optional method for use with Log.
+type Marker interface {
+	Mark(i, j int) string
+}
+
 // Log wraps sort.Interface, sending debug messages to the supplied Writer.
 // Less and Swap parameters will be space-padded based on the most recent Len
 // call. Since writes are not synchronized, a serializing writer should be
 // provided when used with concurrent sorting algorithms. A single *Log can be
 // reused between separate sorts as long as they do not coincide.
+//
+// If the sort.Interface value implements Marker, Mark will be called for Less
+// and Swap if Len() returned a small enough value.
 type Log struct {
-	I sort.Interface
-	W io.Writer
-	p int
+	I    sort.Interface
+	W    io.Writer
+	n, p int
+}
+
+// LOG_ITEM_THRESH is the maximum Len of a sort.Interface that will be printed
+// inline with log messages. If negative, inline display of the data will be
+// disabled.
+var LOG_ITEM_THRESH = 26
+
+func (l *Log) Mark(i, j int) string {
+	if m, ok := l.I.(Marker); ok {
+		return m.Mark(i, j)
+	}
+	return fmt.Sprint(l.I)
 }
 
 func (l *Log) Len() int {
 	r := l.I.Len()
-	l.p = 0
+	l.n, l.p = r, 0
 	if r > 0 {
 		l.p = len(fmt.Sprint(r - 1))
 	}
-	fmt.Fprint(l.W, "(", l.I, ").Len() [", r, "]\n")
+	if r <= LOG_ITEM_THRESH {
+		fmt.Fprint(l.W, "(", l.I, ").Len() [", r, "]\n")
+	} else {
+		fmt.Fprint(l.W, "Len() [", r, "]\n")
+	}
 	return r
 }
 
 func (l *Log) Less(i, j int) bool {
 	r := l.I.Less(i, j)
-	fmt.Fprintf(l.W, "(%v).Less(%*d, %*d) [%v]\n", l.I, l.p, i, l.p, j, r)
+	if l.n <= LOG_ITEM_THRESH && l.n > 0 {
+		fmt.Fprintf(l.W, "(%v).Less(%*d, %*d) [%v]\n", l.Mark(i, j), l.p, i, l.p, j, r)
+	} else {
+		fmt.Fprintf(l.W, "Less(%*d, %*d) [%v]\n", l.p, i, l.p, j, r)
+	}
 	return r
 }
 
 func (l *Log) Swap(i, j int) {
-	v := fmt.Sprint(l.I)
+	if l.n > LOG_ITEM_THRESH || l.n <= 0 {
+		l.I.Swap(i, j)
+		fmt.Fprintf(l.W, "Swap(%*d, %*d)\n", l.p, i, l.p, j)
+		return
+	}
+	v := l.Mark(i, j)
 	l.I.Swap(i, j)
-	fmt.Fprintf(l.W, "(%v).Swap(%*d, %*d) [%v]\n", v, l.p, i, l.p, j, l.I)
+	fmt.Fprintf(l.W, "(%v).Swap(%*d, %*d) [%v]\n", v, l.p, i, l.p, j, l.Mark(i, j))
 }
 
 func (l *Log) String() string {
