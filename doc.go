b3880a41621cbe6d20255827fe26c15ecf519e70
Kevin Gillette <extemporalgenome@gmail.com>
2013-01-24 15:34:21 -0700

    Added package-level documentation.

---
+++ doc.go
@@ -0,0 +1,11 @@
+// Copyright 2013 Kevin Gillette. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package sortutil provides utility functions for generically performing
+// operations on implementors of sort.Interface, such as reversing or
+// shuffling elements. It also provides tools to aid in designing and debugging
+// sorting algorithms that are API-compatible with sort.Sort. Analyze provides
+// a zero-setup method of quickly verifying the correctness and observing the
+// behavior of sorting algorithms.
+package sortutil
